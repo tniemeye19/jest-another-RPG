4d64fdd723b7586be943f48143cdaa7e2cb2c04f
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-10-12 12:39:33 -0400

    created a new Character constructor function. Updated Player and Enemy to inherit methods from Character. Converted all constructor functions to ES6 classes. Updated Player and Enemy to inherit the ES6 way.

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -3,8 +3,6 @@ const Potion = require('../lib/Potion')
 
 jest.mock('../lib/Potion.js');
 
-console.log(new Potion());
-
 test('creates a player object', () => {
     const player = new Player('Dave');
 
